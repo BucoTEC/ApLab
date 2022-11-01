7f0c514bdbfd5745c94e0943c34d07b2a963c8ab
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-01 13:26:19 +0100

    add basic layout content

--- ap-lab-angular-01/src/app/app.module.ts
+++ ap-lab-angular-01/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestButtonComponent } from './test-button/test-button.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestButtonComponent
+    TestButtonComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
