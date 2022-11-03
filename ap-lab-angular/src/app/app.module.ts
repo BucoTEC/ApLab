5c92cf0244f70ca25a2cd294d9fdad041edee10d
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-03 13:43:25 +0100

    cleen up

--- ap-lab-angular/src/app/app.module.ts
+++ ap-lab-angular/src/app/app.module.ts
@@ -3,21 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BottomServiceService } from './bottom-service.service';
-import { ChildComponent } from './child/child.component';
-import { ParentComponent } from './parent/parent.component';
-import { ReferenceComponentComponent } from './reference-component/reference-component.component';
-import { TopServiceService } from './top-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChildComponent,
-    ParentComponent,
-    ReferenceComponentComponent,
-  ],
+  declarations: [],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [TopServiceService, BottomServiceService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
