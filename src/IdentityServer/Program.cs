dd59ffa40d768630fe1f7c36d94e60f64378650e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-01 16:24:48 +0200

    start identity server configuration"

--- src/IdentityServer/Program.cs
+++ src/IdentityServer/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddIdentityServer() .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddTestUsers(TestUsers.Users)
+                .AddDeveloperSigningCredential();
+
+                //TODO finish identity server configuration
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseIdentityServer();
 
 app.Run();
