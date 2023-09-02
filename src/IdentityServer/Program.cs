c3b233a10c33abe9f63122d10970e266a9657cb2
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-02 11:58:47 +0200

    fix dp

--- src/IdentityServer/Program.cs
+++ src/IdentityServer/Program.cs
@@ -1,19 +1,21 @@
 using IdentityServer4.Models;
 using IdentityServer4.Test;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddIdentityServer()
                 .AddInMemoryClients(new List<Client>())
-                .AddInMemoryApiScopes(new List<ApiScope>())
                 .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddInMemoryApiScopes(new List<ApiScope>())
                 .AddTestUsers(new List<TestUser>())
                 .AddDeveloperSigningCredential();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
 
+app.UseRouting();
 app.UseIdentityServer();
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
