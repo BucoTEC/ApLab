46ef086c4e352c339baee6bd3cdb04b17863f277
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-13 00:26:06 +0200

    add test controller

---
+++ src/Api/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello world";
+        }
+    }
+}
