19bd3542311218f9b415f5930277483e303dd52a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-13 16:51:28 +0200

    add idnetity client

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using IdentityModel.Client;
+
+Console.WriteLine("Hello, World from my identity client");
+// discover endpoints from metadata
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
