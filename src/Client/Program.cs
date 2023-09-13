940d3e2512f38db2df8d56f0a6b4f8e47be09328
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-13 17:06:41 +0200

    update token reader

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Hello, World from my identity client");
 // discover endpoints from metadata
 var client = new HttpClient();
 var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+Console.WriteLine(disco.ToString());
 if (disco.IsError)
 {
     Console.WriteLine(disco.Error);
