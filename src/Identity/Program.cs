3e818d10f26a7a8d71cc5aaba4311f979b6aa216
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-13 17:02:48 +0200

    update hosting extension

--- src/Identity/Program.cs
+++ src/Identity/Program.cs
@@ -11,10 +11,6 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddIdentityServer()
-           .AddInMemoryApiScopes(Config.ApiScopes)
-           .AddInMemoryClients(Config.Clients);
-
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .Enrich.FromLogContext()
