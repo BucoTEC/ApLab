fe6233f17d34e90f6f7a58c7fbcdaa6dd18837f7
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-09-14 15:35:32 +0200

    access token req

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -11,3 +11,21 @@ if (disco.IsError)
     Console.WriteLine(disco.Error);
     return;
 }
+
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.AccessToken);
