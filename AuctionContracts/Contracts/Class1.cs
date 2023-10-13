00540d0fe99d97d6e756183c49a2109afd4ce72c
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-10-13 16:08:05 +0200

    setup basic class

---
+++ AuctionContracts/Contracts/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Contracts;
+
+public class Class1
+{
+    /// <summary>
+    /// Used to say hi to a developer in need
+    /// </summary>
+    /// <returns>string</returns>
+    public static string SayHi()
+    {
+        Console.WriteLine("Hello world");
+        return "Hello my friend";
+    }
+}
