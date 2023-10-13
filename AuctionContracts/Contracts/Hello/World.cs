799f45f2d70e1fccb91f3d2c1676992e4a30d947
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-10-14 00:31:44 +0200

    update structure

---
+++ AuctionContracts/Contracts/Hello/World.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contracts.Hello
+{
+    public class World
+    {
+        /// <summary>
+        /// Used to say hi to a developer in need
+        /// </summary>
+        /// <returns>string</returns>
+        public static string SayHi()
+        {
+            Console.WriteLine("Hello world");
+            return "Hello my friend";
+        }
+    }
+}
